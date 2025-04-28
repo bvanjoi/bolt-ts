e44547939bc45405c953fb620667802dd6e42586
bohan <bohan-zhang@foxmail.com>
2025-04-28 20:51:31 +0800

    is top sig

--- tests/cases/compiler/stringReplaceAll/index.ts
+++ tests/cases/compiler/stringReplaceAll/index.ts
@@ -3,3 +3,14 @@
 let a = "abcde".replaceAll("hello");
 //~^ ERROR: Expected 2 arguments, but got 1.
 let b = "abcde".replaceAll("hello", "world");
+
+{
+  type P<T> = { [KeyType in keyof T as KeyType]: PP<T[KeyType]> };
+  type PP<T> = T extends (...arguments_: any[]) => unknown ? T : P<{ [Key in keyof T]: never }>;
+  type TestingType = {
+    tuple: [{ propertyA: string }];
+  };
+  function f(bar: P<TestingType>) {
+    const _a: TestingType = bar;
+  }
+}
