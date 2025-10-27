27e75370a631024f421ba1c41dd7c0dc9a069b4e
bohan <bohan-zhang@foxmail.com>
2025-10-27 23:45:56 +0800

    check: compare index access type and others

--- tests/compiler/errorForUsingPropertyOfTypeAsType02/index.ts
+++ tests/compiler/errorForUsingPropertyOfTypeAsType02/index.ts
@@ -4,5 +4,6 @@ namespace Test1 {
     function foo<T extends { abc: number }>(x: T) {
         let a: T.abc = x.abc;
         //~^ ERROR: Cannot access 'T.abc' because 'T' is a type, but not a namespace. Did you mean to retrieve the type of the property 'abc' in 'T' with 'T["abc"]'?
+        let a0: T['abc'] = x.abc;
     }
 }
